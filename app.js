deaf92936211b9eb479363efedb182affc51d059
motikoma <motikoma5125@gmail.com>
2020-03-23 23:35:52 +0900

    クイズページの実装

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = require("./router/index");
+const app = express();
+const PORT = 3001;
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.use("/", router);
+app.listen(PORT);
